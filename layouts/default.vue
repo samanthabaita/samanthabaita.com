268f8c8bf892b6e69c30fc75ba9aa19e6be60358
Samantha Baita <baita.samantha@gmail.com>
2021-07-06 15:14:38 +0200

    Update banner style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-900 text-white">
-    <div class="container bg-white p-2">
+    <div class="container mx-auto rounded bg-gray-200 p-2">
       <div class="flex items-center justify-start">
         <p class="text-black">👀 A Revas stiamo assumendo! <a href="https://www.revas.io">Scopri di più</a></p>
       </div>
